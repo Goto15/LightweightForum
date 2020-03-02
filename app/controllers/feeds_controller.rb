48693b450f13668baa39a70d23f5f47dd943cc77
GoTo15 <github email address>
2020-03-02 17:33:33 -0600

    Add Feed route

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,2 +1,3 @@
 class FeedsController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :new, :show, :create, :edit, :update]
   resources :topics, only: [:index, :show]
   resources :users, only: [:show, :new, :edit, :update]
+
+  get '/users/:id/feed', to: 'users#feed', as: 'user_feed'
 end
