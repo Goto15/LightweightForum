81dba494455ef4d5f60690d09ea7ec63db4e7723
GoTo15 <github email address>
2020-03-02 16:51:49 -0600

    Adds more Post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,42 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
+        @post = find_post
     end
 
     def new
         @post = Post.new
     end
 
+    def edit
+        @post = find_post
+    end
+
     def create
+        @post = Post.new(post_params)
+
+        if @post.valid?
+            @post.save
+            redirect_to post_path(@post)
+        else
+            puts @post.errors.full_messages
+            redirect_to :action => "new"
+        end
+    end
+
+    def update
+        @post = find_post
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
+    private 
+
+    def find_post
+        Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :content, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :new, :show]
+  resources :posts, only: [:index, :new, :show, :create, :edit, :update]
   resources :topics, only: [:index, :show]
   resources :users, only: [:show, :new, :edit]
 end
